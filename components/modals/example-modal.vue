cd859e8848d572987c49b20dc075bf4a9e74c9ce
Lendo <39434663+lendome@users.noreply.github.com>
2024-07-20 13:24:01 +0200

    fuck stone thats why

--- components/modals/example-modal.vue
+++ components/modals/example-modal.vue
@@ -1,30 +1,23 @@
 <template>
   <div
-    class="bg-stone-200 overflow-hidden gap-4 relative p-4 h-screen flex flex-col w-screen sm:h-auto sm:w-auto sm:rounded-lg"
-  >
-    <h1
-      class="text-xl py-1 px-2 bg-stone-500 text-stone-100 rounded-md font-light uppercase"
-    >
+    class="bg-neutral-200 overflow-hidden gap-4 relative p-4 h-screen flex flex-col w-screen sm:h-auto sm:w-auto sm:rounded-lg">
+    <h1 class="text-xl py-1 px-2 bg-neutral-500 text-neutral-100 rounded-md font-light uppercase">
       Modal
     </h1>
-    <p class="text-stone-500 max-w-sm p-2 text-sm">
+    <p class="text-neutral-500 max-w-sm p-2 text-sm">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae unde quae
       optio at minus, vel expedita ab sunt, quibusdam nulla inventore itaque
       officiis. Velit.
     </p>
     <div class="flex justify-end gap-2 items-center">
-      <button
-        @click="$root.activeModal = null"
-        class="flex gap-2 items-center text-rose-400 font-normal text-sm transition-all duration-300 hover:gap-3 hover:text-rose-500 px-4 py-1"
-      >
+      <button @click="$root.activeModal = null"
+        class="flex gap-2 items-center text-rose-400 font-normal text-sm transition-all duration-300 hover:gap-3 hover:text-rose-500 px-4 py-1">
         <i class="ph-fill text-lg ph-prohibit"></i>
 
         Cancel
       </button>
-      <button
-        @click="$root.activeModal = null"
-        class="bg-stone-500 flex gap-2 items-center text-stone-200 font-normal text-sm rounded-full transition-all duration-300 hover:bg-stone-500/75 hover:gap-3 hover:text-stone-100 px-4 py-1"
-      >
+      <button @click="$root.activeModal = null"
+        class="bg-neutral-500 flex gap-2 items-center text-neutral-200 font-normal text-sm rounded-full transition-all duration-300 hover:bg-neutral-500/75 hover:gap-3 hover:text-neutral-100 px-4 py-1">
         <i class="ph-fill text-lg ph-egg-crack"></i>
 
         Roger that!
--- components/modules/document-navbar.vue
+++ components/modules/document-navbar.vue
@@ -1,16 +1,13 @@
 <!-- navbar -->
 
 <template>
-  <div class="grid w-full bg-stone-200 py-2 px-4 relative">
+  <div class="grid w-full bg-neutral-200 py-2 px-4 relative">
     <div class="grid grid-cols-3 gap-2 items-center">
-      <router-link to="/" class="text-stone-700 text-2xl font-light">
+      <router-link to="/" class="text-neutral-700 text-2xl font-light">
         {{ $root.info.name }}
       </router-link>
       <div class="flex items-center justify-center gap-2">
-        <router-link
-          to="/page-2"
-          class="text-stone-600 flex gap-2 items-center font-light text-sm uppercase"
-        >
+        <router-link to="/page-2" class="text-neutral-600 flex gap-2 items-center font-light text-sm uppercase">
           <div class="ph-fill ph-egg"></div>
           Page 2
         </router-link>
